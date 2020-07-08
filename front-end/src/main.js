0183e10f88425e77512a7b2bded75111f3d43d5d
任俊燊 <734496343@qq.com>
2020-07-08 20:33:03 +0800

    202078

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,8 +4,10 @@ import router from './common/router'
 import store from './common/store'
 import MintUI from 'mint-ui';
 import 'mint-ui/lib/style.css';
+import MyHttpServer from './common/helpers/index'
 
 Vue.use(MintUI);
+Vue.use(MyHttpServer)
 
 Vue.config.productionTip = false
 
@@ -13,4 +15,18 @@ new Vue({
   router,
   store,
   render: h => h(App),
+  mounted() {
+    setRem();
+  },
 }).$mount('#app')
+
+//设置了rem更好的响应式
+function setRem() {
+  const whdef = 100 / 1920;// 表示1920的设计图,使用100PX的默认值
+  const bodyWidth = document.body.clientWidth;// 当前窗口的宽度
+  const rem = bodyWidth * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
+  document.getElementsByTagName('html')[0].style.fontSize = `${rem}px`;
+  console.log(rem);
+}
+window.addEventListener('load', setRem);
+window.addEventListener('resize', setRem);
--- front-end/src/pages/roomSettings/index.vue
+++ front-end/src/pages/roomSettings/index.vue
@@ -1,20 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>房间设置</h1>
-  </div>
+   <div class="hello">
+        <h1>房间设置</h1>
+        <p>推荐玩法阵容</p>
+        <div class='selectedRole aBorder'>
+          <p>已选择角色</p>
+          <mt-button @click="cutRole(i)" class="roleBorder" size="small" type="primary" v-for='(item,i) in yrolelist' :key="i">{{item}}</mt-button>
+        </div>
+        <div class='selectedRole'>
+            <p>点击按钮添加角色</p>
+            <mt-button @click="addRole(item)" class="roleBorder" size="small" v-for='(item,i) in rolelist' :key="i">{{item}}</mt-button>
+        </div>
+        <mt-button type="danger" class="creatRole" @click="creatRole()">创建角色</mt-button>
+    </div>
 </template>
 
 <script>
 // 任俊燊
 export default {
-  name: 'RoomSettings',
-  props: {
-    msg: String
-  }
-}
+ data() {
+    return {
+      yrolelist: ['平民', '预言家', '女巫', '狼人', '白狼王', '守卫', '猎人', '白狼王', '白痴'],
+      rolelist: ['平民', '预言家', '女巫', '狼人', '白狼王', '守卫', '猎人', '白狼王', '白痴'],
+      countedNames:{}
+    };
+  },
+  created() {
+    
+  },
+ methods: {
+    addRole(ev) {
+      console.log(ev)
+      this.yrolelist.push(ev)
+    },
+    cutRole(ev) {
+      console.log(ev)
+      this.yrolelist.splice(ev,1)
+      this.countedNames = this.yrolelist.reduce(function (allNames, name) { 
+          if (name in allNames) {
+            allNames[name]++;
+          }
+          else {
+            allNames[name] = 1;
+          }
+          return allNames;
+        },{});
+        console.log(this.countedNames)
+    },
+  async creatRole() { 
+       console.log(1)
+      // const res = await this.$http.post('https://af6g9a.fn.thelarkcloud.com/createRoom',this.countedNames);
+      // console.log(res);
+
+  //     const {
+  //             meta: {msg, status}
+  //           } = res.data
+  //     if(status === 200) {
+  //        this.$router.push({name: 'home'})
+  //        this.$message.success(msg)
+  //     } else {
+  //       this.$message.error(msg)
+  //     }
+   }
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+p {
+  margin: 0.8rem;
+}
+.hello {
+  width: 100%;
+  /* font-size: 1rem; */
+}
+.roleBorder {
+  width: 25%;
+  height: 2rem;
+  margin: 0.5rem;
+  font-size: 1rem;
+}
+.selectedRole {
+  margin: 10px auto;
+  width: 90%;
+  /* height: 12rem; */
+}
+.aBorder {
+  border: 1px solid black;
+  border-radius:0.5rem;
+  
+}
+.creatRole {
+  width: 100%;
+  font-size: 1rem;
+}
+
+    
+
 
 </style>
